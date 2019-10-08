7d3563ddf485c75900678db2f505243d5be29efa
Shabbadoo26 <38171710+Shabbadoo26@users.noreply.github.com>
2019-10-08 11:08:27 -0400

    added q-and-a

---
+++ q-and-a.js
@@ -0,0 +1,31 @@
+/*
+*  Your task is to implement a question and answer system.
+*  Given a list of questions and answers, prompt the user
+*  to answer each question. If the user answers incorrectly,
+*  ask again until the correct answer is provided.
+*
+*  Use window.prompt and window.alert to request
+*  and provide user feedback.
+*
+*  Feel free to take some liberties in your implementation
+*  for example telling the user they were correct/incorrect,
+*  numbering the questions, indicating to the user how many
+*  questions are remaining, etc.
+*/
+
+// You can modify these arrays for testing, and build objects
+// maps, etc based off of the arrays, but your program
+// should function with different array elements
+// In other words, I will modify the elements in these arrays
+// and your program should still function!
+
+var questions = [
+  'What keyword can be used to declare a variable?',
+  'Object is a primitive type in javascript. (true/false)',
+  'What loop is best for interating over arrays?'
+];
+var answers = ['var','false','for'];
+
+// YOUR CODE STARTS HERE
+
+// YOUR CODE ENDS HERE
